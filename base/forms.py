426208891b3f5783b24c2b589822191fb21cb08e
ElliotSameh <elliotsameh@gmail.com>
2025-07-31 13:26:57 -0400

    Chat Room Messages CRUD

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
-from .models import Room
+from .models import Room, Message
+from django import forms
 
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 4, 'style': 'width:100%;'})
+        }
